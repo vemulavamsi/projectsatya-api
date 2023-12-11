018ba7c6ea2a970ef5bdc694567149228a990818
vamsi <120555540+vemulavamsi@users.noreply.github.com>
2023-12-11 12:56:10 -0500

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'Pycube123$',
     port: 3306,
     database : 'vamsi_project'
 });
